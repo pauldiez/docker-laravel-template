11f63e4e4e1ba48fd430698e44966d6dd30cafb3
Paul Diez <paul.diez@gmail.com>
2022-08-25 14:59:18 +0800

    upgrading laravel

--- src/project.com/resources/views/whatsapp.blade.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<body style="background-color: #1a202c; color: #FFFFFF">
-hi
-</body>
-</html>
--- src/project.com/routes/web.php
+++ src/project.com/routes/web.php
@@ -14,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/message', function () {
-
-    return view('whatsapp');
+    return view('app');
 });
